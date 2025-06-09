6eba97ee3522be79bcc24890fc1b8ef24aef192e
Jerome Maunga <jerhomemaunga@gmail.com>
2025-06-09 09:57:29 +0300

    Change

---
+++ fns_and_dsa/arithmetic_operation.py
@@ -0,0 +1,15 @@
+def perform_operation (num1, num2, operation):
+
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            raise ValueError("Cannot divide by zero.")
+        return num1 / num2
+    else:
+        raise ValueError("Operation not recognized. Use 'add', 'subtract', 'multiply', or 'divide'.")
+    
