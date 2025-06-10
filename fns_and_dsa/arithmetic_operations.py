11c999cbf79e5cb1b65fabc0aac8d7385aca4ea9
Jerome Maunga <jerhomemaunga@gmail.com>
2025-06-10 12:10:19 +0300

    Some few changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,5 @@
 def perform_operation (num1, num2, operation):
-    
-    """
-    Perform an arithmetic operation on two numbers.
 
-    Parameters:
-    num1 (float): The first number.
-    num2 (float): The second number.
-    operation (str): The operation to perform ('add', 'subtract', 'multiply', 'divide').
-
-    Returns:
-    float: The result of the operation.
-    
-    Raises:
-    ValueError: If the operation is not recognized or if division by zero is attempted.
-    """
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
