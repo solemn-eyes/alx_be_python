9951c1bf29b45add58b959659479d9f57c03246b
Jerome Maunga <jerhomemaunga@gmail.com>
2025-06-14 19:10:26 +0300

    A little change

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,4 +18,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"💰 Current Balance: ${self.__account_balance:.2f}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
