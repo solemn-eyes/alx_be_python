fd35cf151a563b6865cdccb3700a3e66c90047b6
Jerome Maunga <jerhomemaunga@gmail.com>
2025-06-14 19:26:11 +0300

    Division Calculator with Commands Line Arguments

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+
+        result = numerator / denominator
+        return f" The result of the division is {result}"
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Invalid input. Please enter numeric values."
+    
+
+    
