5ee76f60ade8b6282f733e4505db4c892564b5bb
Jerome Maunga <jerhomemaunga@gmail.com>
2025-06-14 19:39:05 +0300

    Unit tests for a calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,12 +6,12 @@ class TestSimpleCalculator(unittest.TestCase):
         """Create an instance of SimpleCalculator before each test."""
         self.calculator = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         """Test the addition method."""
         self.assertEqual(self.calculator.add(2, 3), 5)
         self.assertEqual(self.calculator.add(-1, 1), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calculator.subtract(5, 3), 2)
         self.assertEqual(self.calculator.subtract(3, 5), -2)
