c1f025cdf79f54ef581fd71206ae3ddce478416c
Jerome Maunga <jerhomemaunga@gmail.com>
2025-06-17 18:23:02 +0300

    A Change

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,14 +12,14 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"{super().__str__()}, EBook, File Size: {self.file_size}KB"
+        return f"{super().__str__()}, EBook File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
-        return f"{super().__str__()}, Print Book, Page Count: {self.page_count}"
+        return f"{super().__str__()}, Page Count: {self.page_count}"
     
     
 class Library:
